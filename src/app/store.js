e8a1b02c01c05abec82d3dfb29b5db63518646e3
Ebuilt19 <Ediaz191@gmail.com>
2020-11-24 02:10:25 -0500

    redux connected with backend

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import getIdReducer from '../features/getId/getIdSlice'
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    getId: getIdReducer,
   },
 });
